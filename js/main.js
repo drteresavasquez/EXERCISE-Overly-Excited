68e3544fb86d3c1f62f1af09e027d07d16894368
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-01-17 13:57:53 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,30 +16,23 @@ function addExcitement (theWordArray) {
       // outputs the words.
      for (let i = 0; i < theWordArray.length; i++){
 
-     		// This cl prints each individual word to the console
-     		// console.log(theWordArray[i]);
-
-			let currentWord = theWordArray[i];
-     		// like above, this cl prints each individual word to the console
-     		// console.log(currentWord);
-
-     		//this runs through each word adding a space and the next word in the array to the next until the whole sentence is displayed.
-     		output += " " + currentWord;
+     	// This cl prints each individual word to the console
+	let currentWord = theWordArray[i];
+     	
+     	//this runs through each word adding a space and the next word in the array to the next until the whole sentence is displayed.
+     	output += " " + currentWord;
      
-
-			//this one is a doozie...it means if i (the iteration) + 1 (because the index starts at 0) divided by 3 equals 0, then add an exclamation point after that word. 
-     		if ((i+1) % 3===0) {
-     			output += exclamation;
-
-     		//this adds the extra exclamation points with every occurence of the third word.
-     			exclamation += "!";
-     		}
-     		console.log(output);
+	//this one is a doozie...it means if i (the iteration) + 1 (because the index starts at 0) divided by 3 equals 0, then add an exclamation point after that word. 
+     	if ((i+1) % 3===0) {
+     		output += exclamation;
+		//this adds the extra exclamation points with every occurence of the third word.
+     		exclamation += "!";
      	}
+     	console.log(output);
+     }
 
 }
 // Invoke the function and pass in the array
-
 addExcitement(sentence);
 // addExcitement(sentence2);
 
